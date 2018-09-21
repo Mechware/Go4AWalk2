21e3d5dd9f3616e892ac50de84d5fe91e6d06560
twistedburger <twistedburger@hotmail.com>
2018-09-21 15:37:33 -0700

    Item importer for all your item importing needs

--- Assets/Scripts/EventSystem/GameEvents/General/GameEventEditor.cs
+++ Assets/Scripts/EventSystem/GameEvents/General/GameEventEditor.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using CustomeEvents;
+using CustomEvents;
 
 [CustomEditor(typeof(GameEvent))]
 public class GameEventEditor : Editor {
--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -14,13 +14,28 @@ namespace G4AW2.Data.DropSystem
         AMAZING = 5
     }
 
+    public enum ItemType
+    {
+        Material = 0,
+        Consumable = 1,
+        Weapon = 2,
+        Hat = 3,
+        Torso = 4,
+        Boots = 5,
+        Accessory = 6
+    }
+
     [CreateAssetMenu(menuName = "Data/DropSystem/Item")]
     public class Item : ScriptableObject {
+        public AnimationClip Walking;
         public Sprite image;
         public float value;
         public string description;
         public Rarity rarity;
+        public ItemType type; 
 
     }
+
+  
 }
 
