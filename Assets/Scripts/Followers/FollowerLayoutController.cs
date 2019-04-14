104d778c48728e4d19832d6c5970bbc630f4487e
Michael Demone <m_d_emone@hotmail.com>
2019-04-13 21:58:08 -0700

    Fixed the follower layout business

--- Assets/Scripts/Followers/FollowerLayoutController.cs
+++ Assets/Scripts/Followers/FollowerLayoutController.cs
@@ -3,23 +3,12 @@ using UnityEngine;
 
 public class FollowerLayoutController : MonoBehaviour {
 
-    // Use this for initialization
-    void Awake() {
-        transform.hasChanged = true;
-    }
 
+    public FollowerDisplayController FollowerController;
     public float StartX = 16;
     public float StartY = 32;
     public float XGap = 32;
     public float YGap = 0;
-
-    // Update is called once per frame
-    void Update() {
-        if(transform.hasChanged) {
-            transform.hasChanged = false;
-            ChangeLayout();
-        }
-    }
 
 
     [ContextMenu("Apply Layout")]
@@ -27,15 +16,12 @@ public class FollowerLayoutController : MonoBehaviour {
 
         if (transform.childCount == 0) return;
 
-        Transform child = transform.GetChild(transform.childCount - 1);
-        float distBetween = child.GetComponent<FollowerDisplay>().Data.SpaceBetweenEnemies;
-
         float x = StartX;
         float y = StartY;
 
-        for(int i = transform.childCount-1; i >= 0; i--) {
-            RectTransform rectChild = (RectTransform) transform.GetChild(i);
-            distBetween = rectChild.GetComponent<FollowerDisplay>().Data.SpaceBetweenEnemies;
+        foreach(FollowerDisplay followerDisplay in FollowerController.AllFollowers) {
+            RectTransform rectChild = (RectTransform) followerDisplay.transform;
+            int distBetween = followerDisplay.Data.SpaceBetweenEnemies;
 
             Vector3 pos = rectChild.anchoredPosition;
             pos.x = x - distBetween / 2;
